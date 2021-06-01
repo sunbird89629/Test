82f5d178279f0391e36d6815828409954437743d
Howard <wh_sunbird@163.com>
2021-06-01 11:37:24 +0800

    test rxjava

---
+++ app/src/main/java/com/sunbird/test/coroutine/RxDownloadTest.kt
@@ -0,0 +1,31 @@
+package com.sunbird.test.coroutine
+
+import io.reactivex.rxjava3.core.Flowable
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.File
+
+/**
+ * 作者：王豪
+ * 日期：2021/5/31
+ * 描述：<必填>
+ */
+
+
+sealed class DownloadStatus {
+    object None : DownloadStatus()
+    class Progress(val value: Int) : DownloadStatus()
+    class Error(val throwable: Throwable) : DownloadStatus()
+    class Done(val file: File) : DownloadStatus()
+}
+
+
+fun download(url: String, fileName: String): Flowable<DownloadStatus> {
+    return Flowable.create<DownloadStatus>({
+        val request = Request.Builder().url(url).get().build()
+        val okHttpClient = OkHttpClient.Builder().build()
+        val response = okHttpClient.newCall(request).execute()
+        if(response.isSuccessful){
+        }
+    })
+}
