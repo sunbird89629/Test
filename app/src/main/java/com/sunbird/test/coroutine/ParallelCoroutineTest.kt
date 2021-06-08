aeaf650ff80daff34008da3c2f9ebbf390a7c916
Howard <wh_sunbird@163.com>
2021-06-08 20:26:10 +0800

    协程

---
+++ app/src/main/java/com/sunbird/test/coroutine/ParallelCoroutineTest.kt
@@ -0,0 +1,18 @@
+package com.sunbird.test.coroutine
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+
+/**
+ * 作者：王豪
+ * 日期：2021/6/8
+ * 描述：<必填>
+ */
+
+
+//suspend fun fetchTwoDocs() = coroutineScope {
+//    val deferredOne = async { fetchDoc(1) }
+//    val deferredTwo = async { fetchDoc(2) }
+//    deferredOne.await()
+//    deferredTwo.await()
+//}
