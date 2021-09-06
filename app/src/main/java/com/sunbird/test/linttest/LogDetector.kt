463a8ba70717c85cf33bde58d79e777a53823f49
Hao Wang <22340991@qq.com>
2021-09-07 00:05:50 +0800

    add log detector

---
+++ app/src/main/java/com/sunbird/test/linttest/LogDetector.kt
@@ -0,0 +1,13 @@
+package com.sunbird.test.linttest
+
+/**
+ * 作者：王豪
+ * 日期：2021/8/5
+ * 描述：实现了Serializable接口的类，引用类型成员变量也必须要实现Serializable接口
+ */
+//class SerializableClassDetector : BaseDetector(),Detector.UastScanner {
+//
+//  override fun visitClass(context: JavaContext, declaration: UClass) {
+//
+//  }
+//}
