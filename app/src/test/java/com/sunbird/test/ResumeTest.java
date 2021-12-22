cc957b681cccafd7b3c037bdbd0f74bb9ae32619
Hao Wang <22340991@qq.com>
2021-12-22 23:15:32 +0800

    create nianli junit test

---
+++ app/src/test/java/com/sunbird/test/ResumeTest.java
@@ -0,0 +1,37 @@
+package com.sunbird.test;
+
+import org.junit.Test;
+
+/**
+ * 作者：王豪
+ * 日期：2021/12/22
+ * 描述：<必填>
+ */
+
+class ResumeTest {
+    @Test
+    public void testAverageCard() {
+        int[] target = {4, 3, 1, 7, 1};
+        int w = 2;
+
+        //不小于W   average >= W
+
+
+    }
+
+    public void check(int[] target, int w, int index) {
+
+    }
+
+    @Test
+    public void testFindX() {
+        int[] target = {6, 7, 1, 3, 5};
+        int xNumber = 3;
+
+        int xIndex = findX(target, 0, target.length, xNumber);
+    }
+
+    public int findX(int[] target, int start, int end, int xNumber) {
+        
+    }
+}
