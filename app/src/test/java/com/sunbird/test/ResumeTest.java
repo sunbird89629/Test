717c6efffbf9695f8cd8b18513e2f0a3b0820e0f
Hao Wang <22340991@qq.com>
2021-12-23 20:18:34 +0800

    test average card work and right

--- app/src/test/java/com/sunbird/test/ResumeTest.java
+++ app/src/test/java/com/sunbird/test/ResumeTest.java
@@ -1,10 +1,15 @@
 package com.sunbird.test;
 
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 作者：王豪
@@ -13,20 +18,31 @@ import java.util.List;
  */
 
 public class ResumeTest {
+    /**
+     * 均富卡
+     */
     @Test
     public void testAverageCard() {
-        int[] target = {4, 3, 1, 7, 1};
-        int w = 2;
+//        int[] target = {11, 9, 11, 9};
+//        int w = 4;
+//        int expected = 4;
 
-        //不小于W   average >= W
+//        int[] target = {5, 1, 2, 1};
+//        int w = 3;
+//        int expected=2;
 
+        int[] target = {4, 3};
+        int w = 5;
+        int expected = 0;
 
+        int maxNumCount = getMaxNumberCount(target, w);
+        Assert.assertEquals(expected, maxNumCount);
     }
 
-    public void check(int[] target, int w, int index) {
 
-    }
-
+    /**
+     * 构造矩形
+     */
     @Test
     public void testBuildRectangle() {
         int n = 4;
@@ -39,7 +55,55 @@ public class ResumeTest {
     }
 
 
-    private char[][] buildSourceRectangle(int size, Point firstAsterisk, Point secondAsterisk) {
+    /**
+     * 构造排列
+     */
+    @Test
+    public void testBuildArrangement() {
+
+    }
+
+
+    /**
+     * 寻找元素X
+     */
+    @Test
+    public void testFindX() {
+//        int[] source = {6, 7, 1, 3, 5};
+//        int target = 3;
+//        int expect = 4;
+
+//        int[] source = {6, 7, 8, 9, 1, 3, 5};
+//        int target = 3;
+//        int expect = 6;
+
+        int[] source = {3, 4, 7, 1};
+        int target = 2;
+        int expect = -1;
+
+        int xIndex = findX(source, target);
+        Assert.assertEquals(expect, xIndex);
+    }
+
+
+    private int getMaxNumberCount(int[] source, int target) {
+        float currAmount = 0;
+        Arrays.sort(source);
+        for (int i = source.length - 1; i >= 0; i--) {
+            currAmount += source[i];
+            float currAverage = currAmount / (source.length - i);
+            if (currAverage < target) {
+                return source.length - i - 1;
+            } else {
+                continue;
+            }
+        }
+        return source.length;
+    }
+
+
+    @NonNull
+    private char[][] buildSourceRectangle(int size, @NotNull Point firstAsterisk, @NotNull Point secondAsterisk) {
         if (size < 2) {
             throw new RuntimeException("size should great than 2");
         }
@@ -55,6 +119,10 @@ public class ResumeTest {
         }
         if (!checkValid(secondAsterisk.getY(), size)) {
             throw new RuntimeException(String.format(errMessageFormatter, "secondAsterisk.y", secondAsterisk.getY(), size));
+        }
+
+        if (firstAsterisk.equals(secondAsterisk)) {
+            throw new RuntimeException("two Points should not overlap");
         }
 
 //        if (firstAsterisk.getX() == secondAsterisk.getX()) {
@@ -120,6 +188,7 @@ public class ResumeTest {
      * @param asterisk asterisks point
      * @return a rectangle char array
      */
+    @NonNull
     private char[][] buildRectangle(int size, Point[] asterisk) {
         char[][] result = new char[size][size];
 
@@ -141,7 +210,7 @@ public class ResumeTest {
         return value >= 0 && value <= maxValue;
     }
 
-    private void logRectangle(char[][] rectangle) {
+    private void logRectangle(@NonNull char[][] rectangle) {
         StringBuilder sb = new StringBuilder();
         sb.append(">>>>>>>>>>>>>\n");
         for (int i = 0; i < rectangle.length; i++) {
@@ -154,47 +223,35 @@ public class ResumeTest {
         System.out.println(sb.toString());
     }
 
-
-    @Test
-    public void testBuildArrangement() {
-
-    }
-
-    @Test
-    public void testFindX() {
-
-        //1,3,5,6,7
-//        int[] target = {1, 3, 5, 6, 7};
-//        int[] target = {6, 7, 1, 3, 5};
-        int[] source = {6, 7, 8, 9, 1, 3, 5};
-        int target = 3;
-
-        int xIndex = findX(source, target);
-        Assert.assertEquals(5, xIndex);
-    }
-
-    private int findX(int[] source, int target) {
+    private int findX(@NonNull int[] source, int target) {
         int low = 0;
-        int high = source.length;
+        int high = source.length - 1;
 
-        while (low < high) {
+        while (low <= high) {
             int midIndex = (low + high) >> 1;
             int midValue = source[midIndex];
             //should reverse binary search
-            boolean shouldReverse = midValue > source[low];
+//            boolean shouldReverse = midValue > source[low];
+
+
             if (target < midValue) {
-                if (!shouldReverse) {
-                    high = midIndex;
+                if (source[low] > target) {//reverse binary search
+                    low = midIndex + 1;
                 } else {
-                    low = midIndex;
+                    high = midIndex - 1;
                 }
+//                if (!shouldReverse) {
+//                    high = midIndex - 1;
+//                } else {
+//                    low = midIndex + 1;
+//                }
             } else if (midValue == target) {
-                return midIndex;
-            } else {
-                if (!shouldReverse) {
-                    low = midIndex;
+                return midIndex + 1;
+            } else {//midValue<target
+                if (source[high - 1] < target) {//reverse binary search
+                    high = midIndex - 1;
                 } else {
-                    high = midIndex;
+                    low = midIndex + 1;
                 }
             }
         }
@@ -217,5 +274,18 @@ class Point {
 
     public int getY() {
         return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
